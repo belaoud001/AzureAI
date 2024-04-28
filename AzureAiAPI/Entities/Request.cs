6c22939c745f494d65f4e4b7eaa685b340e04ee6
- Belaoud - <abdelhalimbelaoud@gmail.com>
2024-04-28 16:43:56 +0200

    [AzureAiAPI] Support string enum conversion for serialization

--- AzureAiAPI/Entities/Request.cs
+++ AzureAiAPI/Entities/Request.cs
@@ -1,11 +1,17 @@
-﻿namespace AzureAiAPI.Entities;
+﻿using System.Text.Json.Serialization;
+using AzureAiAPI.Enums;
+
+namespace AzureAiAPI.Entities;
 
 public class Request
 {
     
-    public string?    EncodedSource { get; set; }
-    public string?    Text          { get; set; }
-    public bool?      Translate     { get; set; }
     public IFormFile? AudioSource   { get; set; }
+    
+    public string? EncodedSource { get; set; }
+    public string? Text          { get; set; }
+    
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public AzureAiOperation? AzureAiOperation { get; set; }
     
 }
