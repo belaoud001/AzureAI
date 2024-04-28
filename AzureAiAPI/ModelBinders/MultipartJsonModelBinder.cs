0ba6cd8d89814928d3b07470a9adad55eddc52a3
- Belaoud - <abdelhalimbelaoud@gmail.com>
2024-04-28 14:38:47 +0200

    [AzureAiAPI] Add custom model binders & controller config

---
+++ AzureAiAPI/ModelBinders/MultipartJsonModelBinder.cs
@@ -0,0 +1,48 @@
+﻿using AzureAiAPI.Entities;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace AzureAiAPI.ModelBinders;
+
+public class MultipartJsonModelBinder : IModelBinder
+{
+    
+    public async Task BindModelAsync(ModelBindingContext bindingContext)
+    {
+        if (bindingContext == null)
+        {
+            throw new ArgumentNullException(nameof(bindingContext));
+        }
+
+        var valueProviderResult = bindingContext.ValueProvider.GetValue(bindingContext.ModelName);
+        if (valueProviderResult != ValueProviderResult.None)
+        {
+            bindingContext.ModelState.SetModelValue(bindingContext.ModelName, valueProviderResult);
+            return;
+        }
+
+        var httpContext = bindingContext.HttpContext;
+        var request     = httpContext.Request;
+
+        // Check if the request has multipart/form-data content
+        if (request.HasFormContentType)
+        {
+            var form  = await request.ReadFormAsync();
+            var model = new Request
+            {
+                AudioSource = form.Files.Count > 0 ? form.Files[0] : null
+            };
+
+            bindingContext.Result = ModelBindingResult.Success(model);
+            return;
+        }
+
+        // Check if the request has JSON content
+        if (request.HasJsonContentType())
+        {
+            var model = await request.ReadFromJsonAsync<Request>();
+            
+            bindingContext.Result = ModelBindingResult.Success(model);
+        }
+    }
+    
+}
---
+++ AzureAiAPI/ModelBinders/MultipartJsonModelBinderProvider.cs
@@ -0,0 +1,25 @@
+﻿using AzureAiAPI.Entities;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace AzureAiAPI.ModelBinders;
+
+public class MultipartJsonModelBinderProvider : IModelBinderProvider
+{
+    
+    public IModelBinder GetBinder(ModelBinderProviderContext context)
+    {
+        if (context == null)
+        {
+            throw new ArgumentNullException(nameof(context));
+        }
+
+        var modelType = context.Metadata.UnderlyingOrModelType;
+        if (modelType == typeof(Request))
+        {
+            return new MultipartJsonModelBinder();
+        }
+
+        return null;
+    }
+    
+}
